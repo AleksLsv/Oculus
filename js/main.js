30c913a3912a800588d31a155a9b72f24012df4c
Aleksandr <jooksja1@gmail.com>
2023-11-23 01:01:02 +0200

    section game html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
-$(function(){
+'use strick'
 
+// find all elements with classes  '' 
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+function removeClass(elementsList, classToRemove) {
+  elementsList.forEach(function (element) {
+    element.classList.remove(classToRemove);
+  });
+}
+
+function addClass(className, classToAdd) {
+  className.classList.add(classToAdd);
+}
+
+
+
+// apply function 'open' for each element from list by click
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
 });
+
+// definition for function open
+function open(evt) {
+  const tabTarget = evt.currentTarget; // clicked button
+  const buttonValue = tabTarget.dataset.button; // return value of data-button
+
+  
+  removeClass(tabItem, 'tabs__btn-item--active');
+  removeClass(tabContent, 'tabs__content-item--active');
+  // remove class by each element from lists tabItem and tabContent
+  
+
+  addClass(tabTarget, 'tabs__btn-item--active'); // add class to clicked 
+  addClass(document.querySelector(`#${buttonValue}`), 'tabs__content-item--active');
+  // find element by id (# = id) and add a class to this element 
+};
