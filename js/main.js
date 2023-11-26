6ef4dfc8b5d85c40d5dd9b812f5e65cccd3c2c2f
Aleksandr <jooksja1@gmail.com>
2023-11-26 21:50:53 +0200

    adaptive

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function addClass(className, classToAdd) {
 
 
 // apply function 'open' for each element from list by click
-tabItem.forEach(function(element) {
+tabItem.forEach(function (element) {
   element.addEventListener('click', open);
 });
 
@@ -26,13 +26,20 @@ function open(evt) {
   const tabTarget = evt.currentTarget; // clicked button
   const buttonValue = tabTarget.dataset.button; // return value of data-button
 
-  
+
   removeClass(tabItem, 'tabs__btn-item--active');
   removeClass(tabContent, 'tabs__content-item--active');
   // remove class by each element from lists tabItem and tabContent
-  
+
 
   addClass(tabTarget, 'tabs__btn-item--active'); // add class to clicked 
   addClass(document.querySelector(`#${buttonValue}`), 'tabs__content-item--active');
   // find element by id (# = id) and add a class to this element 
 };
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+});
